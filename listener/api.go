732bf4d9af7e05033b0cae7bcc6f28071e952525
stupachek <alona.stupak@gmail.com>
2022-10-28 18:24:05 +0300

    task a

--- listener/api.go
+++ listener/api.go
@@ -26,7 +26,18 @@ func (l *GenericListener[T]) AddChan(ch <-chan T) {
 }
 
 func (l *GenericListener[T]) Listen(ctx context.Context) <-chan T {
-	//TODO: implement listen functionality
+	for _, chanel := range l.chArr {
+		go func(c <-chan T) {
+			for {
+				select {
+				case el := <-c:
+					l.out <- el
+				case <-ctx.Done():
+					return
+				}
+			}
+		}(chanel)
+	}
 
 	return l.out
 }
