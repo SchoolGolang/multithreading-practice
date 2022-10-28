a3eb1fdad069e182d077e7764323a8906213dee2
stupachek <alona.stupak@gmail.com>
2022-10-28 18:24:56 +0300

    task c

--- processor/ph.go
+++ processor/ph.go
@@ -2,6 +2,8 @@ package processor
 
 import (
 	"context"
+	"fmt"
+
 	droneRepository "github.com/SchoolGolang/multithreading-practice/drone/repository"
 	"github.com/SchoolGolang/multithreading-practice/plant/repository"
 	"github.com/SchoolGolang/multithreading-practice/sensor"
@@ -26,5 +28,23 @@ func NewPHProcessor(
 }
 
 func (p *PHProcessor) RunProcessor(ctx context.Context) {
-	//TODO: implement process functionality
+	for {
+		select {
+		case sensorPlant := <-p.input:
+			if sensorPlant.PlantID == "" {
+				continue
+			}
+			plant := p.plantsRepo.GetPlant(sensorPlant.PlantID)
+			ph := sensorPlant.Data
+			normalLowerPh := plant.NormalLowerPh
+			normalUpperPh := plant.NormalUpperPh
+			normalPh := (normalLowerPh + normalUpperPh) / 2
+			fmt.Println("[PHP Processor] plant: ", plant.Name, plant.ID, "current ph:", ph, "normal [", normalLowerPh, ", ", normalUpperPh, "]")
+			if ph < normalLowerPh || normalUpperPh < ph {
+				p.dronesRepo.AdjustSoils(plant.ID, normalPh)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
 }
